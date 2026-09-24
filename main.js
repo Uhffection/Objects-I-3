86549379e36c9d37ecc9ea518504687adb95476c
Uhffection <142746146+Uhffection@users.noreply.github.com>
2026-09-24 17:08:31 +0000

    create object 'newObject' and function 'removeValue'

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
 // Write your code below
+var newObject = {
+    "testKey" : "testPair",
+}
+
+function removeValue(obj,key) {
+    obj[key] = undefined;
+}
+
+removeValue(newObject,"testKey")
+
+console.log(newObject["testKey"])
